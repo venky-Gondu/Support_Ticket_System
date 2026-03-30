87400f53c5ba670b6d33e46916c68913e288cffc
venkatesh21mis7170 <venkatesh.21mis7170@vitapstudent.ac.in>
2026-03-30 16:18:57 +0530

    project Structure setup and configured  required Depedencies.

---
+++ src/main/java/com/Project/Support_Ticket_System/SupportTicketSystemApplication.java
@@ -0,0 +1,13 @@
+package com.Project.Support_Ticket_System;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SupportTicketSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SupportTicketSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Project/Support_Ticket_System/SupportTicketSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Project.Support_Ticket_System;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SupportTicketSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
